ba1072097c4e6e8659d8ca98af52a27fec2aa0ba
Thibault René <thibault38150@gmail.com>
2022-02-17 10:13:46 -0500

    [+] EJS, favicon, index

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-var express = require('express');
-var app = express();
+const express = require('express')
+const bodyParser = require('body-parser');
+const favicon = require('serve-favicon');
 
-// '/' est la route racine
-app.get('/', function (req, res) {
-  res.send('Bonjour !');
+const app = express()
+const port = 80
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(favicon(__dirname + '/public/images/favicon.ico'));
+
+app.get('/', function(req, res) {
+    res.render('index', {capteurs:capteurs});
 });
 
-app.listen(80, function () {
-  console.log("Application d'exemple écoutant sur le port 4000 !");
-});
+app.listen(port, () => {
+  console.log(`Page 1 : http://localhost`)
+})
+
+const capteurs = [
+  {nom:"Capteur1", temp:25, minTemp:18, maxTemp:22, hotStart:17, coldStart:23},
+  {nom:"Capteur2", temp:22, minTemp:18, maxTemp:22, hotStart:17, coldStart:23},];
